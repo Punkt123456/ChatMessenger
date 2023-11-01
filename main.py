73c943cdebd945ca8c2b9514ea49d5d27d71df2d
Hussein Kronboch <hussein.kronboch1@gmail.com>
2023-11-01 19:32:16 +0100

    Flask_Webaaplication

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from crypt import methods
+from email import message
+from pyexpat.errors import messages
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route("/")
-def start_page():
-    return "<h1>Hello, World!</h1><a href='https://google.de'>Google</a>"
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(200), nullable=True)
+    content = db.Column(db.String(500), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+@app.route("/<name>", methods=['GET', 'POST'])
+def start_page(name):
+    if request.method == 'POST':
+        new_message = Message(
+            user = name,
+            content = request.form['content']
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        messages = Message.query.order_by(Message.created_at).all()
+    return render_template('index.html', messages=messages)
 
 if __name__ =="__main__":
     app.run(debug=True)
